4f81b7950dfd7458039d432d5ce662fcecf5b940
KarKar100 <morkiral@gmail.com>
2021-06-21 16:27:04 +0300

    Initial project version

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Diana on 21.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test/cars.swift
@@ -0,0 +1,16 @@
+//
+//  cars.swift
+//  Test
+//
+//  Created by Diana on 21.06.2021.
+//
+
+import Foundation
+class CAR {
+    var carWeight: Int?
+    var carColor: String?
+    let txt = "ЕДУ"
+    func Drive () {
+        print(txt)
+    }
+}
